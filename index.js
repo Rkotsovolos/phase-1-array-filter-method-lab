6c28d8f33c336801afe180e9e05060f5c42cc6a1
Ryan Kotsovolos <rkotsovolos@gmail.com>
2021-10-14 15:21:56 -0700

    commit tree 47b378fad7a1afac6d70604ce9dbcf678b596701

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(array, string) {
+    const driver = array.filter(drivers => drivers.toUpperCase() === string.toUpperCase())
+    return driver;
+}
+
+function fuzzyMatch(array, string) {
+    const driver = array.filter(drivers => drivers.startsWith(string))
+    return driver;
+}
+
+function matchName(array, string) {
+    const driver = array.filter(drivers => drivers.name === string)
+    return driver;
+}
